31fdb85ab78e9193a6dbb3cc4449686d623ee235
vishalsiripuram <66208358+vishalsiripuram@users.noreply.github.com>
2023-03-16 20:37:09 +0530

    Update mycontroller.java

--- src/main/java/com/example/jenkins/controller/mycontroller.java
+++ src/main/java/com/example/jenkins/controller/mycontroller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class mycontroller {
     
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String hel(){
         return "hello java1.8 world";
     }
