66afc4ec55101ccbd00418cf337cf42441cbe7c0
vishalsiripuram <66208358+vishalsiripuram@users.noreply.github.com>
2023-03-17 13:36:27 +0530

    Update JenkinsApplication.java

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -2,7 +2,6 @@ package com.example.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class JenkinsApplication  {
